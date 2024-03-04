3d2f8d6ecc625824e29f550a75f62bb362b01348
wtRinaldi <williamtrinaldi@gmail.com>
2024-03-03 17:21:54 -0800

    adds about me section

--- pages/about.vue
+++ pages/about.vue
@@ -1,4 +1,48 @@
 <template>
-  <div>About Us</div>
+  <div class="max-w-xl mx-auto pb-7">
+    <h2>About Me:</h2>
+    <p>
+      Hey there! I'm William Rinaldi, a passionate front-end developer with a love for Vue.js and Nuxt.js. With a keen eye for design and a knack for creating seamless user experiences, I thrive on turning ideas into reality through the power of code.
+    </p>
+
+    <h2>What Drives Me:</h2>
+    <p>
+      My journey in web development began with a fascination for creating visually stunning and highly functional websites. As I delved deeper into the world of front-end development, Vue.js and Nuxt.js quickly became my go-to tools. Their simplicity, flexibility, and robust ecosystem empower me to build dynamic web applications with ease.
+    </p>
+
+    <h2>Why Vue and Nuxt:</h2>
+    <p>
+      Vue.js's elegant syntax and reactive data binding make it a joy to work with, while Nuxt.js's framework provides structure and Server Side Rendering to my projects. Together, they enable me to craft responsive and performant web applications that not only meet but exceed client expectations.
+    </p>
+
+    <h2>My Approach:</h2>
+    <p>
+      In every project I undertake, my goal is to deliver not just a product, but an experience. I pay meticulous attention to detail, ensuring that every element of the user interface is intuitive, visually appealing, and optimized for performance across various devices and screen sizes.
+    </p>
+
+    <h2>Continuous Learning:</h2>
+    <p>
+      As a front-end developer, I believe in the importance of continuous learning and staying updated with the latest trends and technologies in the ever-evolving landscape of web development. Whether it's mastering new features in Vue.js, exploring innovative design patterns, or honing my skills in responsive web design, I am committed to expanding my knowledge and pushing the boundaries of what's possible.
+    </p>
+
+    <h2>Let's Collaborate:</h2>
+    <p>
+      If you're looking for a front-end developer who is passionate about creating beautiful and functional web experiences using Vue.js and Nuxt.js, I'd love to hear from you. Let's collaborate and bring your ideas to life in the digital world!
+    </p>
+
+    <h2>Connect with Me:</h2>
+    <p>
+      Feel free to reach out to me via williamtrinaldi@gmail.com or <a href="https://www.linkedin.com/in/wtrinaldi/" class="font-bold text-[#0a66c2]">LinkedIn</a> to discuss your project ideas or just to geek out about Vue.js and Nuxt.js. I'm always excited to connect with fellow developers and enthusiasts who share my passion for front-end development!
+    </p>
+  </div>
 </template>
 
+<style scoped>
+  h2 {
+    @apply font-bold text-xl pb-4 pt-7
+  }
+
+  p {
+    @apply text-gray-900
+  }
+</style>
