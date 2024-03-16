ed8808c7cbb9349984efaf76f7e840823d4dad00
wtRinaldi <williamtrinaldi@gmail.com>
2024-03-15 17:36:57 -0700

    adds dvh to page views

--- pages/ui-elements/index.vue
+++ pages/ui-elements/index.vue
@@ -1,11 +1,11 @@
 <template>
   <div class="snap-y snap-mandatory h-dvh overflow-scroll no-scrollbar">
-    <div class="bg-sky-400 h-screen snap-center">
+    <div class="bg-sky-400 h-dvh snap-center">
       <ElementsEmailSubscribeCard class="snap-center" />
     </div>
-    <div class="bg-emerald-300 h-screen snap-center"></div>
-    <div class="bg-orange-400 h-screen snap-center"></div>
-    <div class="bg-gray-900 h-screen snap-center"></div>
+    <div class="bg-emerald-300 h-dvh snap-center"></div>
+    <div class="bg-orange-400 h-dvh snap-center"></div>
+    <div class="bg-gray-900 h-dvh snap-center"></div>
   </div>
 </template>
 
@@ -18,5 +18,5 @@ useHead({
   }]
 })
 
-
 </script>
+
