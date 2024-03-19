aec97d218eff3c9e427f07f01541efa7ab1d6c0f
wtRinaldi <williamtrinaldi@gmail.com>
2024-03-19 10:11:53 -0700

    adds new icons to manifest

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -66,37 +66,43 @@ export default defineNuxtConfig({
       display: 'standalone',
       icons: [
         {
-          src: 'icon-48-48.png',
+          src: 'maskable_icon_x48.png',
           sizes: '48x48',
           type: 'image/png',
           purpose: 'any'
         },
         {
-          src: 'icon-72-72.png',
+          src: 'maskable_icon_x72.png',
           sizes: '72x72',
           type: 'image/png',
           purpose: 'any'
         },
         {
-          src: 'icon-96-96.png',
+          src: 'maskable_icon_x96.png',
           sizes: '96x96',
           type: 'image/png',
           purpose: 'any'
         },
         {
-          src: 'icon-144-144.png',
+          src: 'maskable_icon_x128.png',
           sizes: '144x144',
           type: 'image/png',
           purpose: 'any'
         },
         {
-          src: 'icon-192-192.png',
+          src: 'maskable_icon_x192.png',
           sizes: '192x192',
+          type: 'image/png',
+          purpose: 'any'
+        },
+        {
+          src: 'maskable_icon_x384.png',
+          sizes: '384x384',
           type: 'image/png',
           purpose: 'maskable'
         },
         {
-          src: 'icon-512-512.png',
+          src: 'maskable_icon_x512.png',
           sizes: '512x512',
           type: 'image/png',
           purpose: 'any'
