686aad2eec17da2072dbdb5f853efaad67c61e4b
wtRinaldi <williamtrinaldi@gmail.com>
2024-03-29 11:42:19 -0700

    makes nav option bigger on mobile devices

--- components/Navigation.vue
+++ components/Navigation.vue
@@ -1,19 +1,19 @@
 <template>
   <div class="w-full absolute left-0 bg-white lg:hidden transition-transform ease-in-out duration-500 z-40"
        :class="[isShowMenu ? 'translate-y-14' : '-translate-y-72']">
-    <nav class="mt-6 ml-12 mb-12">
-      <ul class="flex flex-col gap-4">
-        <li class="py-3">
-          <NuxtLink to="/about" @click="closeMenu" class="flex">About</NuxtLink>
+    <nav class="mt-6 mx-12 mb-10">
+      <ul class="flex flex-col gap-2">
+        <li>
+          <NuxtLink to="/about" @click="closeMenu" class="mobile-navigation-option">About</NuxtLink>
         </li>
-        <li class="py-3">
-          <NuxtLink to="/experience" @click="closeMenu" class="flex">Experience</NuxtLink>
+        <li>
+          <NuxtLink to="/experience" @click="closeMenu" class="mobile-navigation-option">Experience</NuxtLink>
         </li>
-        <li class="py-3">
-          <NuxtLink to="/blog" @click="closeMenu" class="flex">Blog</NuxtLink>
+        <li>
+          <NuxtLink to="/blog" @click="closeMenu" class="mobile-navigation-option">Blog</NuxtLink>
         </li>
-        <li class="py-3">
-          <NuxtLink to="/ui-elements" @click="closeMenu" class="flex">UI Elements</NuxtLink>
+        <li>
+          <NuxtLink to="/ui-elements" @click="closeMenu" class="mobile-navigation-option">UI Elements</NuxtLink>
         </li>
       </ul>
     </nav>
@@ -63,6 +63,10 @@ function closeMenu() {
 <style scoped>
 .router-link-exact-active {
   @apply text-active
+}
+
+.mobile-navigation-option {
+  @apply flex items-center h-16 hover:bg-quinary rounded-full pl-6;
 }
 
 </style>
