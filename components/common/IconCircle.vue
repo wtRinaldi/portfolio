e20c3d1e62963da05534b27fade48e2d42c723c8
wtRinaldi <williamtrinaldi@gmail.com>
2024-03-30 19:25:38 -0700

    shrink homepage cards

--- components/common/IconCircle.vue
+++ components/common/IconCircle.vue
@@ -1,5 +1,5 @@
 <template>
-  <NuxtIcon :name class="flex justify-center items-center h-11 w-11 text-2xl bg-secondary-light rounded-full text-white" />
+  <NuxtIcon :name class="flex justify-center items-center h-10 w-10 text-2xl bg-secondary-light rounded-full text-white" />
 </template>
 <script setup>
 defineProps({
--- components/summary/Card.vue
+++ components/summary/Card.vue
@@ -1,12 +1,12 @@
 <template>
-  <div class="flex flex-col min-h-80 rounded-3xl shadow-xl bg-white w-full">
-    <div class="flex flex-col justify-center items-center uppercase pt-8 pb-6 text-primary-light font-bold tracking-wide border-b">
+  <div class="flex flex-col min-h-72 rounded-3xl shadow-xl bg-white w-full">
+    <div class="flex flex-col justify-center items-center uppercase py-5 text-primary-light tracking-wide border-b">
       <div class="flex w-full">
         <CommonIconCircle :name="icon" class="ml-6" />
-        <div class="flex items-center text-lg uppercase px-6 font-medium">{{ label }}</div>
+        <div class="flex items-center text-lg uppercase pl-4 font-medium">{{ label }}</div>
       </div>
     </div>
-    <div class="flex items-center px-8 mx-3 mb-3 rounded-b-xl leading-relaxed text-lg flex-1">
+    <div class="flex items-center px-8 mx-3 rounded-b-xl leading-relaxed text-lg flex-1">
       <slot />
     </div>
   </div>
