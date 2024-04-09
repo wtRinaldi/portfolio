63fc59305d4d552cb5d74523c211b51d7691f3bd
wtRinaldi <williamtrinaldi@gmail.com>
2024-04-09 11:25:27 -0700

    Revert "adds leveraging team blog"
    This reverts commit f562fc13d7db83a4ff237912c85223ab4b36ad1e.

--- components/content/Paragraph.vue
+++ components/content/Paragraph.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="pb-4">
+  <div class="pb-6">
     <slot />
   </div>
 </template>
--- pages/blog/index.vue
+++ pages/blog/index.vue
@@ -15,7 +15,7 @@
             <div class="flex flex-col justify-between items-end">
               <div class="flex text-white rounded-full text-sm font-semibold bg-secondary-light pt-0.5 px-2">
                 <NuxtIcon :name="article.type" class="text-xl mr-1.5" />
-                <span>{{ getArticleLabel(article.type) }}</span>
+                <span>{{ article.type }}</span>
               </div>
               <p class="text-sm font-semibold text-gray-500 mr-1">{{ article.date }}</p>
             </div>
@@ -28,14 +28,5 @@
 
 <script setup>
 const query = { path: '/blog', sort: [{ date: -1 }] }
-
-function getArticleLabel (type) {
-  if (type === 'soft-skills') {
-    return 'Soft Skills'
-  } else {
-    return type
-  }
-
-}
 
 </script>
