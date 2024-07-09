3dc9c55c99e82876dac3fcaefa83288bd73758ab
wtRinaldi <williamtrinaldi@gmail.com>
2024-07-09 12:37:35 -0700

    update hero spacing

--- components/hero/Hero.vue
+++ components/hero/Hero.vue
@@ -1,25 +1,26 @@
 <template>
-  <section class="flex flex-col md:flex-row mt-12 md:mt-20 justify-center">
-<!--    <NuxtImg src="/mountains_1920x1281.jpg"-->
-<!--             class="h-64 w-64 md:h-96 md:w-96 max-h-160 max-w-96 mb-24 md:mb-0 rounded-full order-2 md:order-1 mx-auto"-->
-<!--             alt="mountains"-->
-<!--             loading="eager"-->
-<!--             width="1920"-->
-<!--             height="1280"-->
-<!--             sizes="sm:255px md:320px lg:480px xl:1280px"-->
-<!--             quality="80"-->
-<!--             format="webp"-->
-<!--             preload-->
-<!--    />-->
-    <div class="flex justify-center">
+  <section class="flex flex-col md:flex-row mt-8 md:mt-20 justify-center">
+    <div class="flex justify-center mb-6">
       <DotLottieVue style="height: 300px; width: 300px" autoplay loop speed="0.9" src="/animation.lottie"/>
     </div>
 
     <div class="flex flex-col justify-center items-center xl:pl-0 order-1 md:order-2 max-w-xl">
       <h1 class="font-bold text-6xl mb-6">Hi!</h1>
       <h2 class="font-medium text-2xl mb-4">I'm William Rinaldi</h2>
-      <p class="mb-24 md:mb-0 text-center">An experienced in front-end developer. Skilled in HTML, CSS, JavaScript, React, and Vue. Strong collaborator focused on creating responsive, performant, user-friendly, and secure web applications.</p>
+      <p class="mb-24 md:mb-0 text-center">An experienced in front-end developer. Skilled in HTML, CSS, JavaScript, React, and Vue. Strong collaborator focused on creating responsive, user-friendly, and performant web applications.</p>
     </div>
+
+<!--        <NuxtImg src="/mountains_1920x1281.jpg"-->
+<!--                 class=" mb-24 md:mb-0 order-2 md:order-1 mx-auto"-->
+<!--                 alt="mountains"-->
+<!--                 loading="eager"-->
+<!--                 width="1920"-->
+<!--                 height="1280"-->
+<!--                 sizes="sm:255px md:320px lg:480px xl:1280px"-->
+<!--                 quality="80"-->
+<!--                 format="webp"-->
+<!--                 preload-->
+<!--        />-->
   </section>
 </template>
 
