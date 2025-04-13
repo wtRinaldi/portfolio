dec325269575ca828a9713b0abd74a53bb6cc1d2
wtRinaldi <williamtrinaldi@gmail.com>
2025-04-13 11:23:11 -0700

    adds resume download

---
+++ components/content/DownloadResume.vue
@@ -0,0 +1,14 @@
+<template>
+    <a href="/WilliamRinaldi_resume.pdf" download class="rounded-full button-style">Download Resume</a>
+</template>
+
+<style scoped>
+.button-style {
+  display: flex;
+  padding: 10px 16px;
+  background-color: black;
+  color: white;
+  text-decoration: none;
+  height: fit-content;
+}
+</style>
--- pages/experience.vue
+++ pages/experience.vue
@@ -1,8 +1,11 @@
 <template>
   <NuxtLayout name="contained-padded">
     <ContentDoc :path="`/experience`" v-slot="{ doc }">
-      <article class="max-w-xl mx-auto">
-        <h1 class="page-header">experience</h1>
+      <article class="max-w-xl mx-auto relative">
+        <div class="flex justify-between">
+          <h1 class="page-header">experience</h1>
+          <DownloadResume class="md:mt-3" />
+        </div>
         <ContentRenderer :value="doc" />
       </article>
     </ContentDoc>
