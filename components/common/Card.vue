96ab3856ac45cded574679ac1149e09b373bd783
wtRinaldi <williamtrinaldi@gmail.com>
2025-04-13 20:06:29 -0700

    adds styling to proficiencies

--- components/common/Card.vue
+++ components/common/Card.vue
@@ -1,8 +1,6 @@
 <template>
     <div class="card product-card text-center selectable-card">
-      <div class="flex justify-center pt-4 flex-1">
         <slot />
-      </div>
     </div>
   </template>
   
--- components/technicalProficiencies/TechnicalProficiencies.vue
+++ components/technicalProficiencies/TechnicalProficiencies.vue
@@ -2,17 +2,24 @@
     <section>
         <div class="flex flex-col">
             <h2 class="text-2xl font-medium text-center mb-6">Technical Proficiencies</h2>
-            <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 gap-4 ">
+            <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 gap-4 self-center">
                 <CommonCard v-for="proficiency in proficiencies">
-                    {{ proficiency.name }}
-                    <div>
-                        <NuxtImg 
-                            :src="proficiency.icon"
-                            style="height: 100px; width: 100px;"
-                            class=""
-                            alt="vue logo"
-                            loading="lazy" />
+                    <div class="flex flex-col">
+       
+                            <NuxtImg 
+                                :src="proficiency.icon"
+                                style="height: 75px; width: 75px;"
+                                class="mt-3 mb-4 self-center"
+                                :alt="`${proficiency.name} logo`"
+                                loading="lazy" />
+                                <h3 class="uppercase font-bold">{{ proficiency.name }}</h3>
+                        
+                        <div class="flex justify-between">
+                            
+                            <span>YRS EXP {{ proficiency.experience }}</span>
+                        </div>
                     </div>
+
                 </CommonCard>
             </div>
         </div>
@@ -24,43 +31,58 @@
 const proficiencies = [
     {
         name: 'Vue',
-        icon: '/vue.svg'
+        icon: '/vue.svg',
+        experience: '6'
     },
     {
         name: 'Nuxt',
-        icon: '/nuxt.svg'
+        icon: '/nuxt.svg',
+        experience: '1'
+    },
+    {
+        name: 'Vuex',
+        icon: '/vuex.svg',
+        experience: '5'
     },
     {
         name: 'Pinia',
-        icon: '/pinia.svg'
+        icon: '/pinia.svg',
+        experience: '1'
     },
     {
         name: 'React',
-        icon: '/react.svg'
+        icon: '/react.svg',
+        experience: '2'
     },
     {
         name: 'Angular',
-        icon: '/angular.svg'
+        icon: '/angular.svg',
+        experience: '2'
     },
     {
         name: 'Tailwind',
-        icon: '/tailwind.svg'
+        icon: '/tailwind.svg',
+        experience: '4'
     },
     {
         name: 'Storybook UI',
-        icon: '/storybook.svg'
+        icon: '/storybook.svg',
+        experience: '3'
     },
     {
         name: 'HTML',
-        icon: '/html5.svg'
+        icon: '/html5.svg',
+        experience: '9'
     },
     {
         name: 'CSS',
-        icon: '/css3.svg'
+        icon: '/css3.svg',
+        experience: '9'
     },
     {
         name: 'Plotly JS',
-        icon: '/plotly.svg'
+        icon: '/plotly.svg',
+        experience: '1'
     }
 ]
 
