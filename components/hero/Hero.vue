a0fc8ca9ab38ed3465df32aca15d9120c6db421d
wtRinaldi <williamtrinaldi@gmail.com>
2025-04-22 11:41:10 -0700

    improve spacing hero

--- components/hero/Hero.vue
+++ components/hero/Hero.vue
@@ -1,14 +1,14 @@
 <template>
-  <section class="flex flex-col lg:flex-row items-center justify-between gap-12 py-16 px-6 lg:px-20 bg-gradient-to-br from-white via-gray-50 to-gray-100">
+  <section class="flex flex-col lg:flex-row items-center justify-between gap-12 py-12 px-6 lg:px-20 bg-gradient-to-br from-white via-gray-50 to-gray-100">
     <!-- Text Content -->
     <div class="flex flex-col flex-[2] text-center lg:text-left animate-fadeInUp order-1 lg:order-none">
-      <h1 class="text-5xl md:text-6xl font-extrabold leading-tight mb-4 text-gray-900 tracking-tight">
+      <h1 class="text-6xl md:text-[4.25rem] leading-[4.25rem] font-extrabold leading-tight mb-4 md: mb-8 text-gray-900 ">
         WILLIAM RINALDI
       </h1>
       <h2 class="text-2xl md:text-3xl font-semibold text-blue-600 mb-6">
         Senior Frontend Developer
       </h2>
-      <p class="text-gray-700 text-lg leading-relaxed max-w-xl mx-auto lg:mx-0">
+      <p class="text-gray-700 md:text-lg leading-relaxed max-w-xl mx-auto lg:mx-0">
         An experienced front-end developer, proficient in <span class="font-medium text-gray-900">HTML, CSS, JavaScript, Tailwind, React</span>, and <span class="font-medium text-gray-900">Vue</span>.
         Strong collaborator focused on creating responsive, user-friendly, and performant web applications.
       </p>
